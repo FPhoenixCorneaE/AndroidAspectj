74edc3b829e4e78aa99b39c7758a7c9ed4514187
wangkz <wangkz@infore.com>
2022-01-10 09:29:01 +0800

    build:组件版本升为 “1.0.7”;

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -6,8 +6,8 @@ object Deps {
         const val buildToolsVersion = "31.0.0"
         const val minSdkVersion = 21
         const val targetSdkVersion = 31
-        const val versionCode = 106
-        const val versionName = "1.0.6"
+        const val versionCode = 107
+        const val versionName = "1.0.7"
     }
 
     object FPhoenixCorneaE {
